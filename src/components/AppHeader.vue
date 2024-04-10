dbb1dbd91a244d10574a9e2dd554591035907d37
JoaoVArruda <jv.arrudapercinotto@hotmail.com>
2024-04-09 22:57:03 -0400

    Arquivo inicial da avaliação

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,15 @@
+<template>
+  <header class="header">
+    Header
+  </header>
+</template>
+
+<style>
+  .header {
+    background-color: #000;
+    font-size: 2rem;
+    font-weight: bold;
+    color: #fff;
+    text-align: center;
+  }
+</style>
